9e2a7c24d5a5e73d48a1b8dee815cd48e153a596
RadulescuRichardAndrei <richardradulescu22@gmail.com>
2022-02-22 20:13:33 +0200

    Update homework1.java

--- homework1/src/homework1.java
+++ homework1/src/homework1.java
@@ -10,20 +10,34 @@ public class homework1 {
         if(c.length()>1)
             return false;
         else
-            return Character.isLetter(c.charAt(0));
+            return true;
     }
 
     public static void main(String[] args) {
         int n,p;
-        String[] alph;
+        char[] alph= new char[args.length-2];
         if(validate(args[0]) && validate(args[1])){
             n=Integer.parseInt(args[0]);
             p=Integer.parseInt(args[1]);
             }
         for(int i = 2; i < args.length; i++) {
             if(validateS(argc[i]))
-                alph[i-2]=args[i];
+                alph[i-2]=args[i].charAt(0);
         }
+
+        String[] words = new String[n];
+        for(i = 0; i < n; i++){
+            StringBuilder sb =new StringBuilder();
+            while (true){
+                int pos = (int) (Math.random() * (alph.length+1))-1;
+                if(pos<0) break;
+                sb.append(alph[pos]);
+            }
+            words[i]=sb.toString();
+        }
+
+        
+
 
     }
 }
