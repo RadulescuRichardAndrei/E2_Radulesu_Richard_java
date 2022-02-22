b8416c21d12d26f60644ee5b70764dae111fe123
RadulescuRichardAndrei <richardradulescu22@gmail.com>
2022-02-22 18:34:35 +0200

    Update lab1.java

--- src/lab1.java
+++ src/lab1.java
@@ -1,2 +1,8 @@
 public class lab1 {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        String[] languages= {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        int n = (int) (Math.random() * 1000000) * 3;
+
+    }
 }
